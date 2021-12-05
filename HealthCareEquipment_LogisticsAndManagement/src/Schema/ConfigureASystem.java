0a33bb46c24e38b1429fa73745fe24ffb228c998
Yashwanth-2985604 <avula.ya@northeastern.edu>
2021-12-04 22:38:33 -0500

    PackageDetails Added

--- HealthCareEquipment_LogisticsAndManagement/src/Schema/ConfigureASystem.java
+++ HealthCareEquipment_LogisticsAndManagement/src/Schema/ConfigureASystem.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package System;
+package Schema;
+
 
 import Schema.Employee.Employee;
 import Schema.Role.SysAdminRole;
